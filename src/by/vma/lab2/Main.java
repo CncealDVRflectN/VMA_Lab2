8dcc20a4fe9fe768dd851d7102c1e5aa08482a3b
TTOJlTOPA <zhuk.valeriy.98@gmail.com>
2016-11-02 20:57:16 +0300

    1

---
+++ src/by/vma/lab2/Main.java
@@ -0,0 +1,15 @@
+package by.vma.lab2;
+
+public class Main {
+    public static void main(String[] args) {
+        Square.vma();
+    }
+}
+/*
+Gauss:
+0,99822
+1,99987
+-2,99976
+-9,00001
+6,00705
+ */
